e4a546a97d6c5bbe6750f7b3abbc2b9a53fccefc
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-20 17:44:04 +0700

    fix(objectives): notifyAt nullable (field can be null)

--- app/modules/objective/schemas/create-objective.schema.ts
+++ app/modules/objective/schemas/create-objective.schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const schema = z.object({
     title: z.string().min(1).max(127),
     description: z.string().nullable(),
-    notifyAt: z.string().datetime()
+    notifyAt: z.string().datetime().nullable()
 });
 
 export type createObjectiveSchema = z.infer<typeof schema>;
