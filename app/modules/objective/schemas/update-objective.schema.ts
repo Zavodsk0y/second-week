164785f499dc03a591f3ad549c3e948640a72f2f
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-20 17:40:04 +0700

    fix(objectives): add isCompleted in update schema

--- app/modules/objective/schemas/update-objective.schema.ts
+++ app/modules/objective/schemas/update-objective.schema.ts
@@ -5,7 +5,8 @@ import { uuidObjectiveFSchema } from "../../../common/schemas/uuid-objective.sch
 const schema = z.object({
     title: z.string().min(1).max(127).optional(),
     description: z.string().nullable().optional(),
-    notifyAt: z.string().datetime().optional()
+    notifyAt: z.string().datetime().optional(),
+    isCompleted: z.boolean().optional()
 });
 
 export type updateObjectiveSchema = z.infer<typeof schema>;
