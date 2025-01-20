fefa598b3588ba9f6b5e6f68db8f4bf87cf391c7
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-20 12:55:05 +0700

    feature(objectives): added id field in response schema, created repository with methods (insert, getById), created controller with create method

---
+++ app/modules/objective/controller.objective.ts
@@ -0,0 +1,18 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { sqlCon } from "../../common/config/kysely-config";
+import { HttpStatusCode } from "../../common/enum/http-status-code";
+import * as objectiveRepository from "./repository.objective";
+import { createObjectiveSchema } from "./schemas/create-objective.schema";
+
+export async function create(req: FastifyRequest<{ Body: createObjectiveSchema }>, rep: FastifyReply) {
+    const objective = {
+        title: req.body.title,
+        description: req.body.description,
+        creatorId: req.user.id!,
+        notifyAt: req.body.notifyAt
+    };
+
+    const insertedObjective = await objectiveRepository.insert(sqlCon, objective);
+
+    return rep.code(HttpStatusCode.OK).send({ ...insertedObjective });
+}
---
+++ app/modules/objective/repository.objective.ts
@@ -0,0 +1,12 @@
+import { type Insertable, type Kysely, Transaction } from "kysely";
+import { DB, Objectives } from "../../common/types/kysely/db.type";
+
+type InsertableObjectiveRowType = Insertable<Objectives>;
+
+export async function insert(con: Kysely<DB> | Transaction<DB>, entity: InsertableObjectiveRowType) {
+    return await con.insertInto("objectives").returningAll().values(entity).executeTakeFirstOrThrow();
+}
+
+export async function getById(con: Kysely<DB> | Transaction<DB>, id: string) {
+    return await con.selectFrom("objectives").selectAll().where("id", "=", id).executeTakeFirstOrThrow();
+}
