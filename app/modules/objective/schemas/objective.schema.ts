f6b0e2e1abeadad324a8555466731b951eac8ecc
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-20 12:12:45 +0700

    another check

--- app/modules/objective/schemas/objective.schema.ts
+++ app/modules/objective/schemas/objective.schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const schema = z.object({
     title: z.string().min(1).max(127),
     description: z.string().nullable(),
-    creatorId: z.string(),
+    creatorId: z.string().uuid(),
     notifyAt: z.string().datetime(),
     createdAt: z.string().datetime(),
     updatedAt: z.string().datetime(),
