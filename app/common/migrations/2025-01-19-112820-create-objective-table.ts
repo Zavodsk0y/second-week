76a39042de3d037d7c352814b1eacb661fa53339
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-20 11:34:44 +0700

    refactor(core): change migration names, create objectives migration

---
+++ app/common/migrations/2025-01-19-112820-create-objective-table.ts
@@ -0,0 +1,30 @@
+import { type Kysely, sql } from "kysely";
+
+export async function up(db: Kysely<any>) {
+    await db.schema
+        .createTable("objectives")
+        .addColumn("id", "uuid", (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+        .addColumn("title", "varchar(127)", (col) => col.notNull())
+        .addColumn("description", "text")
+        .addColumn('creatorId', 'uuid', (col) =>
+            col.references('users.id').onDelete('cascade').notNull(),
+        )
+        .addColumn("notifyAt", "timestamp")
+        .addColumn("createdAt", "timestamp", (col) =>
+            col.defaultTo(sql`now()`).notNull())
+        .addColumn("updatedAt", "timestamp", (col) =>
+            col.defaultTo(sql`now()`).notNull())
+        .addColumn("is_completed", "boolean", (col) =>
+            col.defaultTo(sql`false`).notNull())
+        .execute();
+
+    await db.schema
+        .createIndex('user_creator_id_index')
+        .on('objectives')
+        .column('creatorId')
+        .execute()
+}
+
+export async function down(db: Kysely<any>) {
+    await db.schema.dropTable("users").execute();
+}
