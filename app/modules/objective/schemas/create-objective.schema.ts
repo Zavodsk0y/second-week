b16e06d586f8ea7f7b4840aeab5b53aab1bdb24a
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-21 11:27:39 +0700

    fix(objectives): optional notifyAt field

--- app/modules/objective/schemas/create-objective.schema.ts
+++ app/modules/objective/schemas/create-objective.schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const schema = z.object({
     title: z.string().min(1).max(127),
     description: z.string().nullable(),
-    notifyAt: z.string().datetime().nullable()
+    notifyAt: z.string().datetime().nullable().optional()
 });
 
 export type createObjectiveSchema = z.infer<typeof schema>;
