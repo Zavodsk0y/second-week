89db698cc3646920960d11dfe63ed7089a0c6ea2
Zavodsk0y <zimniyvolk5@gmail.com>
2025-01-27 14:14:01 +0700

    refactor(objective): also checking share when user's trying to get todo, not only the creator id, and removed guard for this route cuz some other checking logic

--- app/modules/objective/router.objective.ts
+++ app/modules/objective/router.objective.ts
@@ -8,7 +8,7 @@ import { checkObjectivePolicyGet } from "./utils/check-objective-policy-get";
 
 export const objectiveRouter = async (app: FastifyInstance) => {
     app.post("/", { schema: createObjectiveFSchema }, objectiveController.create);
-    app.get("/:id", { schema: uuidFSchema, preHandler: app.auth([checkObjectivePolicyGet]) }, objectiveController.findOne);
+    app.get("/:id", { schema: uuidFSchema }, objectiveController.findOne);
     app.get("/", { schema: paramsObjectiveFSchema }, objectiveController.findAll);
     app.patch("/:id", { schema: updateObjectiveFSchema, preHandler: app.auth([checkObjectivePolicyGet]) }, objectiveController.update);
 };
--- app/modules/user-objective-share/router.user-objective-share.ts
+++ app/modules/user-objective-share/router.user-objective-share.ts
@@ -7,6 +7,6 @@ import { userObjectiveShareFSchema } from "./schemas/user-objective-share.schema
 export const userObjectiveShareRouter = async (app: FastifyInstance) => {
     app.post("/:id/share", { schema: userObjectiveShareFSchema, preHandler: app.auth([checkObjectivePolicyGet]) }, userObjectiveShareController.create);
     app.delete("/:id/revoke", { schema: userObjectiveShareFSchema, preHandler: app.auth([checkObjectivePolicyGet]) }, userObjectiveShareController.revoke);
-    app.get("/:id/list-grants", { schema: uuidFSchema }, userObjectiveShareController.getGrantedUsersForObjective);
+    app.get("/:id/list-grants", { schema: uuidFSchema, preHandler: app.auth([checkObjectivePolicyGet]) }, userObjectiveShareController.getGrantedUsersForObjective);
     app.get("/shared", {}, userObjectiveShareController.getShared);
 };
