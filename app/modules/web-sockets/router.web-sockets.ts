70a2c301deea5d62b73ef438e8d371817e85302e
Zavodsk0y <zimniyvolk5@gmail.com>
2025-02-04 15:51:17 +0700

    fix(objective): removing jwt checks, enabling authorizatio header & request instance

--- app/modules/web-sockets/router.web-sockets.ts
+++ app/modules/web-sockets/router.web-sockets.ts
@@ -1,24 +1,14 @@
-import { FastifyInstance } from "fastify";
-import jwt from "jsonwebtoken";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import { addConnection, removeConnection } from "./utils/web-sockets-connect";
 
-interface ITokenJwtPayload extends jwt.JwtPayload {
-    id: string;
-    email: string;
-    iat: number;
-    exp: number;
-}
-
 export const webSocketsRouter = async (app: FastifyInstance) => {
-    app.get("/", { websocket: true, config: { isPublic: true } }, (socket) => {
+    app.get("/", { websocket: true }, (socket, req: FastifyRequest) => {
         socket.on("message", (message) => {
             const data = JSON.parse(message.toString());
-            if (data.type === "subscribe" && data.userId && data.token) {
+            if (data.type === "subscribe" && req.headers.authorization!.split(" ")[1]) {
                 try {
-                    const decoded = jwt.verify(data.token, process.env.JWT_SECRET!) as ITokenJwtPayload;
-                    if (decoded.id === data.userId) {
-                        addConnection(data.userId, socket);
-                    }
+                    addConnection(req.user.id!, socket);
+                    console.log("user added!");
                 } catch {
                     socket.close();
                 }
